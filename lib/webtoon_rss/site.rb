5ab4913f19025dcd9b9cf5f0abb4ffe0b3b40f14
ChaYoung You <yousbe@gmail.com>
2014-07-12 11:08:04 +0900

    Use next to skip iteration

--- lib/webtoon_rss/site.rb
+++ lib/webtoon_rss/site.rb
@@ -17,9 +17,8 @@ module WebtoonRSS
 
       fixed_site = site.split("\n")
       resp.errors.reverse.each do |error|
-        if error.message =~ /^htmlParseStartTag/
-          fixed_site[error.line - 1].gsub!(/^([\w\W]{#{error.column - 2}})(<)([^>]*)(>)/, "\\1&lt;\\3&gt;")
-        end
+        next unless error.message =~ /^htmlParseStartTag/
+        fixed_site[error.line - 1].gsub!(/^([\w\W]{#{error.column - 2}})(<)([^>]*)(>)/, "\\1&lt;\\3&gt;")
       end
 
       fixed_site.join("\n")
