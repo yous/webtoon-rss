6dcc2783d1a1b502436d83d07946cfbc9837eeae
ChaYoung You <yousbe@gmail.com>
2014-07-12 09:54:16 +0900

    Set version to 0.1.0

--- lib/webtoon_rss.rb
+++ lib/webtoon_rss.rb
@@ -5,5 +5,6 @@ require 'time'
 require 'nokogiri'
 
 # Internal requires
+require 'webtoon_rss/version'
 require 'webtoon_rss/site'
 require 'webtoon_rss/naver'
---
+++ lib/webtoon_rss/version.rb
@@ -0,0 +1,3 @@
+module WebtoonRSS
+  VERSION = '0.1.0'
+end
